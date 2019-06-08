a2a672feae009e59e3847ce7b1040f6d5bb0d885
Pablo <pablo@Camilas-MacBook-Pro.local>
2019-06-07 21:14:36 -0300

    Writing test javascript code, looked at variables, strings, numbers, arrays and conditions

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var failed;
+var student="Nando";
+
+//Check if student is Pablo and log resulted test
+
+// if(student=="Pablo"){
+//     failed = false;
+//     console.log(student+" failed: " + failed);
+// }else if(student=="Nando") {
+//     failed = true;
+//     console.log(student+" failed: " + failed);
+// }
+// else {
+//     console.log(student+" Failed.")
+// }
+
+var student1=["nando",30,1989,true];
+var student2=["pablo",24,1994,true];
+var student3=["camila",24,1994,false];
+
+// console.log("this student is named " + student1[0] + ", they are " + student1[1] + " years old, born in " + student1[2] + ".")
+// console.log("this student is named " + student2[0] + ", they are " + student2[1] + " years old, born in " + student2[2] + ".")
+// console.log("this student is named " + student3[0] + ", they are " + student3[1] + " years old, born in " + student3[2] + ".")
+
